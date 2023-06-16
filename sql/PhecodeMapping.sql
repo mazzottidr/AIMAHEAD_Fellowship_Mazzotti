f2b71e80b21d7a30a8badffdff43f4c8c2b11060
Diego Mazzotti <droblesmazzotti@kumc.edu>
2023-06-16 11:39:29 -0500

    Update PhecodeMapping.sql

--- sql/PhecodeMapping.sql
+++ sql/PhecodeMapping.sql
@@ -1,6 +1,5 @@
 -- Phecode Mapping
 -- Requires important phecode map from https://phewascatalog.org/phecodes
-
 select
 	o.PATIENT_NUM,
 	o.ENCOUNTER_NUM,
@@ -16,6 +15,19 @@ select top 100 * from S19.dbo.ICD_Phecode_OBS_FACT;
 select count(*) from S19.dbo.ICD_Phecode_OBS_FACT; -- 153026231
 select count(distinct PATIENT_NUM) from S19.dbo.ICD_Phecode_OBS_FACT; -- 3384497
 
+-- only distincnt and for included patients (1.6 million)
+-- drop table S19.dbo.ICD_Phecode_Included
+select distinct *
+into S19.dbo.ICD_Phecode_Included
+from S19.dbo.ICD_Phecode_OBS_FACT
+where PATIENT_NUM in (select PATIENT_NUM from Pat_OSA_INS_Index_Included_Demo_SDOH) and phecode IS NOT NULL;
+
+select top 1000 * from S19.dbo.ICD_Phecode_Included;
+select count(*) from S19.dbo.ICD_Phecode_Included; -- 105475045
+select count(distinct PATIENT_NUM) from S19.dbo.ICD_Phecode_Included; -- 1670021
+
+
+/*
 with temp as (
 select distinct
 	PATIENT_NUM,
@@ -35,5 +47,5 @@ select distinct
 from S19.dbo.ICD_Phecode_OBS_FACT
 )
 select count(distinct PATIENT_NUM) from temp; --3384497
-
+*/
 
